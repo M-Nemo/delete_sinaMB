cbb586a611e9fac35e20262ae4700edee6ff0110
Nemo <smallmama@qq.com>
2016-03-20 00:32:51 +0800

    Update

--- deleteMicroblog.js
+++ deleteMicroblog.js
@@ -1,17 +1,17 @@
-var fEvent = function(element,event){
+var Event = function(element,event){
   var evt = document.createEvent( 'HTMLEvents' );
   evt.initEvent(event, true, true);  
   return !element.dispatchEvent(evt);
 }
 function del(){
 	var menu = document.getElementsByClassName("screen_box")[0];
-	fEvent(menu.children[0],"click");
+	Event(menu.children[0],"click");
 	var delList = document.getElementsByClassName("layer_menu_list");
 	var list = delList[1].children;
 	var btn = list[0].children[0].children[0];
-	fEvent(btn,"click")
+	Event(btn,"click")
 	var e = document.getElementsByClassName("screen_box")[0].children[2].children[0].children[2].children[0];
-	fEvent(e,"click");
+	Event(e,"click");
 }
 
 setInterval(function(){
